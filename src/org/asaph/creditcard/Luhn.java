da0e52dd7c653602aa424eaad80434a3f501f3a5
asaph <ace@dontletgo.com>
2013-05-21 11:16:39 -0700

    Changed full name to github username

--- src/org/asaph/creditcard/Luhn.java
+++ src/org/asaph/creditcard/Luhn.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 Asaph Engel
+ * Copyright 2010 asaph.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 package org.asaph.creditcard;
 
 /**
- * @author Asaph Engel
+ * @author asaph
  *
  */
 public class Luhn implements CheckAlgorithm { 
